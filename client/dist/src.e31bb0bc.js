ab3df91d9650feee3e2915e1d8044364d54ea93c
dreak603 <dreak603@gmail.com>
2019-11-07 21:11:21 +0800

    bug fix

--- client/dist/src.e31bb0bc.js
+++ client/dist/src.e31bb0bc.js
@@ -78343,7 +78343,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "62264" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "49779" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
